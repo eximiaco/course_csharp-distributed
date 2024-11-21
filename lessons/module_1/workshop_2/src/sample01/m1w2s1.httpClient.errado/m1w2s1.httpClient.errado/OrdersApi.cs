2c19f6ac571fc2dea3ef54721b5bf5aad9afc19f
Francisco Leonardo Schneider <francisco.schneider@eximia.co>
2024-11-21 07:14:23 -0300

    HttpClien certo.

---
+++ lessons/module_1/workshop_2/src/sample01/m1w2s1.httpClient.errado/m1w2s1.httpClient.errado/OrdersApi.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace m1w2s1.httpClient.errado;
+
+public class OrdersApi
+{
+    private readonly string _url = "orders_api_uri";
+
+    public async Task<IEnumerable<OrderDto>> GetOrdersAsync(CancellationToken cancellationToken)
+    {
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(_url, cancellationToken).ConfigureAwait(false);
+        var json = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+        return JsonConvert.DeserializeObject<IEnumerable<OrderDto>>(json)!;
+    }
+}
+
+public record OrderDto(int Id, decimal Amount);
