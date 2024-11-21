671a35c6324dbdc179091bc20f6bb867d87e1a6f
Francisco Leonardo Schneider <francisco.schneider@eximia.co>
2024-11-21 11:29:18 -0300

    HTTP Client com Flurl.

---
+++ lessons/module_1/workshop_2/src/sample03/m1w2s3.httpClient.flurl/m1w2s3.httpClient.flurl/Program.cs
@@ -0,0 +1,65 @@
+using Flurl.Http;
+using m1w2s3.httpClient.flurl;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<OrdersApi>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Informo que os status 400 ao 404 e o 422 são aceitáveis e não devem lançar a FlurlHttpException
+// Consequentemente não entra em retentativa
+FlurlHttp.Clients.WithDefaults(settings =>
+{
+    settings.AllowHttpStatus("400-404,422");
+});
+
+app.MapGet("/orders/bearerToken", async (OrdersApi ordersApi, CancellationToken cancellationToken) =>
+{
+    string token = ""; // Token obtido da request ou através de nova autenticação
+    var orders = await ordersApi.GetOrdersWithBearerTokenAsync(token, cancellationToken);
+    return Results.Ok(orders);
+});
+
+app.MapGet("/orders/apiKey", async (OrdersApi ordersApi, CancellationToken cancellationToken) =>
+{
+    string apiKey = ""; // API Key obtida da request ou através de uma configuração
+    var orders = await ordersApi.GetOrdersWithApiKeyAsync(apiKey, cancellationToken);
+    return Results.Ok(orders);
+});
+
+app.MapGet("/orders/clientCredentials", async (OrdersApi ordersApi, CancellationToken cancellationToken) =>
+{
+    string clientId = ""; // Client id e secret obtidos de um vault
+    string clientSecret = "";
+    var orders = await ordersApi.GetOrdersWithClientCredentialsAsync(clientId, clientSecret, cancellationToken);
+    return Results.Ok(orders);
+});
+
+app.MapPost("/orders", async (OrdersApi ordersApi, CancellationToken cancellationToken) =>
+{
+    string token = ""; // Token obtido da request ou através de nova autenticação
+    CreateOrderRequest request = new(250m, DateTime.Now, "Treinamento Eximia");
+    var id = await ordersApi.CreateOrderAsync(request, token, cancellationToken);
+    return Results.Ok();
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
